d4e3c8192260eec7327cec3b6bcddfe511542f52
Micah <micahincorporated@gmail.com>
2019-09-16 17:08:47 -0500

    Update main.py

--- main.py
+++ main.py
@@ -12,16 +12,20 @@ async def on_ready():
 async def on_message(message):
     if message.author == client.user:
         return
-
     if message.content.startswith('%hello'):
         await message.channel.send('Hello!  SqueakyMeme is up and running!')
     if message.content.startswith('%ver'):
-            await message.channel.send('0.1.1') 
+        await message.channel.send('0.1.1') 
     if message.content.startswith("%rec"):
         await message.channel.send("DM micahlt#4835 for your memes to be added to the bot.")
     if message.content.startswith("%info"):
         await message.channel.send("All commands start with a dollar sign.")
         await message.channel.send("Commands: info, ver, hello, rec, and more soon!")
         await message.channel.send("Developed by Micah Lindley")
+    if message.content.startswith("%meme"): 
+        memeNumber = ['0', '1', '2', '3', '4', '5']
+        meme = '0'
+        channel.send(file=discord.File('https://raw.githubusercontent.com/micahlt/sm-bot/master/memes/' + meme + '.jpg'))
+        print('https://raw.githubusercontent.com/micahlt/sm-bot/master/memes/' + meme + '.jpg')
 
 client.run(os.environ['TOKEN'])
