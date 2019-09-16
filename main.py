ff8bf6ddb5b70da3d61eeb1d0830bd432a6f4665
Micah <micahincorporated@gmail.com>
2019-09-16 16:17:50 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
-from discord.ext import commands
 import discord
-import random, os
+import random
 
-client = commands.Bot(command_prefix="$")
+client = discord.Client()
 
 @client.event
 async def on_ready():
@@ -13,24 +12,15 @@ async def on_message(message):
     if message.author == client.user:
         return
 
-    if message.content.startswith('$hello'.lower()):
-        await message.channel.send('Hello!  SqueakyMeme is up and running')
-    if message.content.startswith('$ver'):
-            await message.channel.send('0.1.1')
-    if message.content.startswith("$add".lower()):
-        await message.channel.send("Visit https://squeakymeme.ml/ to add your memes to the bot!")
-    if message.content.startswith("$info".lower()):
+    if message.content.startswith('%hello'):
+        await message.channel.send('Hello!  SqueakyMeme is up and running!')
+    if message.content.startswith('%ver'):
+            await message.channel.send('0.1.1') 
+    if message.content.startswith("%rec"):
+        await message.channel.send("DM micahlt#4835 for your memes to be added to the bot.")
+    if message.content.startswith("%info"):
         await message.channel.send("All commands start with a dollar sign.")
         await message.channel.send("Commands: info, ver, hello, rec, and more soon!")
-        await message.channel.send("Developed by micahlt#4835 and smileycreations15#0215")
-"""
-client.remove_command("help")
-@client.command(name="help")
-async def help(ctx):
-	ctx.channel.send("help")
+        await message.channel.send("Developed by Micah Lindley")
 
-@client.command(name="name")
-async def command(ctx):
-	pass
-"""
-client.run(os.environ['TOKEN'])
+client.run('NjIzMjQ3MTkzOTg1NTE1NTIz.XX_tvw.PsHtPY1Wig68qAA6Xi4I0Rz-gjY')
